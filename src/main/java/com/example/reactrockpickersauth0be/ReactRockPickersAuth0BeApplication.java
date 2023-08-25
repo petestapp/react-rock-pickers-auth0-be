d680ecec4cc243de686abd73409dad06a6a05782
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-25 15:27:17 -0500

    first commit

---
+++ src/main/java/com/example/reactrockpickersauth0be/ReactRockPickersAuth0BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.reactrockpickersauth0be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactRockPickersAuth0BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactRockPickersAuth0BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reactrockpickersauth0be/ReactRockPickersAuth0BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactrockpickersauth0be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactRockPickersAuth0BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
