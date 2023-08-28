778695ef2c1b1ba25a7a2a5dabe5c6488e0d3c60
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-28 11:10:19 -0500

    no arg rock constructor test passing

---
+++ src/main/java/rocks/Rock.java
@@ -0,0 +1,17 @@
+package rocks;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Rock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+    public String type;
+
+    public Rock() {
+    }
+}
