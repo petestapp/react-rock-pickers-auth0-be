8b66ae81e20f6c86b1dfb39235ca50ef1de94f2e
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-29 13:58:06 -0500

    rock service constructor should add default rocks test passing

--- src/main/java/rocks/RockService.java
+++ src/main/java/rocks/RockService.java
@@ -2,12 +2,25 @@ package rocks;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class RockService {
     private RockRepository repository;
 
     public RockService(RockRepository repository) {
         this.repository = repository;
+//        List<Rock> defaultRocks = defaultRocks();
+        repository.saveAll(defaultRocks());
+    }
+
+    public static List<Rock> defaultRocks() {
+        return List.of(
+                new Rock("Obsidian"),
+                new Rock("Basalt"),
+                new Rock("Quartz")
+        );
     }
 
     public Rock saveRock(Rock rock) {
