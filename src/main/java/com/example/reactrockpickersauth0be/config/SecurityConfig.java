06d9406a447e2789bfe479c6d1561db18a36c490
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-30 15:46:13 -0500

    updated build.gradle with oauth2-resource-server and security; reworked cors filter into cors configuration source

--- src/main/java/com/example/reactrockpickersauth0be/config/SecurityConfig.java
+++ src/main/java/com/example/reactrockpickersauth0be/config/SecurityConfig.java
@@ -2,17 +2,20 @@ package com.example.reactrockpickersauth0be.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.util.Arrays;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.addAllowedOrigin("http://localhost:3000");
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "OPTIONS"));
@@ -21,6 +24,6 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
 
-        return new CorsFilter(source);
+        return source;
     }
 }
