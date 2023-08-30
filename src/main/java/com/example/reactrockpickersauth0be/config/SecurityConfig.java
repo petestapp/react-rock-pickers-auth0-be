462d4a723e460b240c19ca1b2add52d0ce3fc319
petestapp <63821853+petestapp@users.noreply.github.com>
2023-08-30 17:34:47 -0500

    security filter chain created; cors configuration test passing

--- src/main/java/com/example/reactrockpickersauth0be/config/SecurityConfig.java
+++ src/main/java/com/example/reactrockpickersauth0be/config/SecurityConfig.java
@@ -2,17 +2,32 @@ package com.example.reactrockpickersauth0be.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/getAllRocks").authenticated()
+                .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .anyRequest().permitAll()
+                .and()
+                .cors(cors -> cors.configurationSource(corsConfigurationSource));
+
+        return http.build();
+    }
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
